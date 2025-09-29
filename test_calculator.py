d5dd9e42b5c63f0b23304053a6ef31e01d3af173
lin3338 <3486755762@qq.com>
2025-09-29 14:33:44 +0800

    test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,8 @@
 import pytest
-2. from calculator import add, multiply
-3.
-4. def test_add():
-5.     assert add(2, 3) == 5
-6.     assert add(-1, 1) == 0
-7.
-8. def test_multiply():
-9.     assert multiply(3, 4) == 12
-10.     assert multiply(2, 0) == 0
+from calculator import add, multiply
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(2, 0) == 0
